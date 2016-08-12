5fffd82e523ffabe170563b4bfde0cf762a05ffd
Alex34467 <borisov.alexey0@gmail.com>
2016-08-12 10:37:49 +0300

    Implemented main method in Main class.

--- src/com/myPackage/Main.java
+++ src/com/myPackage/Main.java
@@ -1,11 +1,22 @@
 package com.myPackage;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 // Main class
 public class Main
 {
     // main method.
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
+        // Input.
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Input URL: ");
+        String input = reader.readLine();
 
+        // Connection.
+        if (Connection.connect(input)) System.out.println("Connection successful.");
+        else System.out.println("Connection error.");
     }
 }
