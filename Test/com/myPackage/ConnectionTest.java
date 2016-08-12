59b2814c6ff0f84c8d18f1504983dc1af97d1cef
Alex34467 <borisov.alexey0@gmail.com>
2016-08-12 09:52:06 +0300

    Created ConnectionTest class.

---
+++ Test/com/myPackage/ConnectionTest.java
@@ -0,0 +1,15 @@
+package com.myPackage;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+// ConnectionTest class.
+public class ConnectionTest
+{
+    @Test
+    public void testConnect() throws Exception
+    {
+
+    }
+}
