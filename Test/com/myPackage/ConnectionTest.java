f02b203883532e2990aa929ff4dfeeb6b26ae148
Alex34467 <borisov.alexey0@gmail.com>
2016-08-12 10:30:59 +0300

    Added testConnect method in ConnectionTest class.

--- Test/com/myPackage/ConnectionTest.java
+++ Test/com/myPackage/ConnectionTest.java
@@ -10,6 +10,10 @@ public class ConnectionTest
     @Test
     public void testConnect() throws Exception
     {
+        String correctURL = "https://www.yandex.ru/";
+        String uncorrectURL = "12345.77";
 
+        assertFalse(Connection.connect(uncorrectURL));
+        assertTrue(Connection.connect(correctURL));
     }
 }
